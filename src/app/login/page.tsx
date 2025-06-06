b118710a401dffc314d8d5e935bb2679479a533e
felip-araujo <felipearaujoexecutive@gmail.com>
2025-06-06 10:29:24 -0400

    login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { useState } from "react"
+import { useRouter } from "next/router"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Card, CardContent } from '@/components/ui/card'
+
+export default function LoginPage() {
+    
+}
